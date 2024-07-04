6bf7916b1da05f4e4c13eb34e1debccc29263b11
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2024-07-04 23:12:08 +0200

    note create progress

--- resources/js/Pages/Note/Create.tsx
+++ resources/js/Pages/Note/Create.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import {Head} from "@inertiajs/react";
+import React, {useState} from 'react';
+import {Head, useForm} from "@inertiajs/react";
 import Layout from "@/Layouts/Layout";
 import {PageProps} from "@/types";
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import InputError from "@/Components/InputError";
 
 const Create = ({auth}: PageProps) => {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        title: "",
+        content: "",
+        tag: []
+    });
+
     return (
         <>
             <Head title="Create Note" />
-            <Layout user={auth}>
-                <div>Create</div>
+            <Layout user={auth.user}>
+                <InputLabel htmlFor="title" value="Title" />
+
+                <TextInput
+                    id="title"
+                    type="text"
+                    name="title"
+                    value={data.title}
+                    className="mt-1 block"
+                    autoComplete="username"
+                    isFocused={true}
+                    onChange={(e) => setData('title', e.target.value)}
+                />
+
+                <InputError message={errors.title} className="mt-2" />
             </Layout>
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         './resources/js/**/*.tsx',
     ],
 
+    darkMode: ['selector', '[data-mode="dark"]'],
+
     theme: {
         extend: {
             height: {
